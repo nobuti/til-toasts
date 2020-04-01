17bedd67019083ae0bffce385c3968fd21863b63
nobuti <buti@nobuti.com>
2020-04-01 17:44:46 +0200

    Css base ground with emotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import {Fragment} from 'react'
+import {Global, jsx, css} from '@emotion/core'
+
+import {reset} from './util/reset'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+    <Global styles={reset} />
+    <section css={css`
+      height: 100vh;
+      background-color: #2548f5;
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}>Wadus</section>
+    </Fragment>
   );
 }
 
---
+++ src/util/reset.js
@@ -0,0 +1,77 @@
+import { css } from '@emotion/core'
+
+export const reset = css`
+/* Box sizing rules */
+html {
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after { 
+  box-sizing: inherit;
+}
+/* Remove default padding and list-styles */
+ul,
+ol {
+  padding: 0;
+  list-style: none;
+}
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ul,
+ol,
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.4;
+  font-family: 'system-ui';
+}
+/* Make images easier to work with */
+img,
+video {
+  height: auto;
+  max-width: 100%;
+  display: block;
+}
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+/* Reset iframe border */
+iframe {
+  border: 0;
+}
+/* Table modern styles */
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+td,
+th {
+  padding: 0;
+}
+td:not([align]),
+th:not([align]) {
+  text-align: left;
+}
+`

ba0599270e00ba0428232d9e335aa70a492e6fdc
nobuti <buti@nobuti.com>
2020-04-02 12:31:21 +0200

    Create node to hold overlays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 /** @jsx jsx */
-import {Fragment} from 'react'
-import {Global, jsx, css} from '@emotion/core'
+import { Fragment, useEffect } from 'react'
+import { Global, jsx, css } from '@emotion/core'
 
-import {reset} from './util/reset'
+import { reset } from './util/reset'
 
 function App() {
+  useEffect(() => {
+    const container = document.querySelector('#overlays')
+    if (!container) {
+      const overlays = document.createElement('div')
+      overlays.setAttribute('id', 'overlays')
+      document.body.appendChild(overlays)
+    }
+  }, [])
+
   return (
     <Fragment>
-    <Global styles={reset} />
-    <section css={css`
-      height: 100vh;
-      background-color: #2548f5;
-      color: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    `}>Wadus</section>
+      <Global styles={reset} />
+      <section
+        css={css`
+          height: 100vh;
+          background-color: #2548f5;
+          color: #fff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        Wadus
+      </section>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App

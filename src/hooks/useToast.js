06768c782a68e9b226b3d860faf295383462c296
nobuti <buti@nobuti.com>
2020-04-02 13:55:35 +0200

    useToast hook

---
+++ src/hooks/useToast.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { v1 as uuid } from 'uuid'
+
+const useToast = () => {
+  const [toasts, updateToasts] = useState([])
+
+  const addToast = (toast) => {
+    const data = {
+      ...toast,
+      toast: uuid(),
+    }
+
+    updateToasts([...toasts, data])
+  }
+
+  const removeToast = (toast) => {
+    const filtered = toasts.filter((t) => t.toast !== toast)
+    updateToasts(filtered)
+  }
+
+  return {
+    addToast,
+    removeToast,
+  }
+}
+
+export default useToast

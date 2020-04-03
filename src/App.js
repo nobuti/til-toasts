19eaf2c330a0b217b3cce3e057cd6f7cb8e14db2
nobuti <buti@nobuti.com>
2020-04-03 19:18:13 +0200

    Include Toasting component
    Decline to use Portal and remove the container node.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 /** @jsx jsx */
-import { useEffect } from 'react'
 import { Global, jsx, css } from '@emotion/core'
 
 import { reset } from './util/reset'
 import { ToastProvider } from './components/toastContext'
 import Card from './components/Card'
+import Toasting from './components/Toasting'
 
-function App() {
-  useEffect(() => {
-    const container = document.querySelector('#overlays')
-    if (!container) {
-      const overlays = document.createElement('div')
-      overlays.setAttribute('id', 'overlays')
-      document.body.appendChild(overlays)
-    }
-  }, [])
+const App = () => (
+  <ToastProvider>
+    <Global styles={reset} />
+    <section
+      css={css`
+        height: 100vh;
+        background-color: #2548f5;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <Card />
+    </section>
 
-  return (
-    <ToastProvider>
-      <Global styles={reset} />
-      <section
-        css={css`
-          height: 100vh;
-          background-color: #2548f5;
-          color: #fff;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
-      >
-        <Card />
-      </section>
-    </ToastProvider>
-  )
-}
+    <Toasting />
+  </ToastProvider>
+)
 
 export default App

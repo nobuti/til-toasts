b19afd593c32e8333044b3c7d15c8d5b0736fef9
nobuti <buti@nobuti.com>
2020-04-04 17:36:51 +0200

    Fine tune animation

--- src/components/Toasting.js
+++ src/components/Toasting.js
@@ -1,31 +1,47 @@
 /** @jsx jsx */
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { jsx, css } from '@emotion/core'
+import { useTransition, animated } from 'react-spring'
 
 import ToastContext from './toastContext'
 import Toast from './Toast'
 
 const Toasting = () => {
   const { toasts } = useContext(ToastContext)
+  const [refMap] = useState(() => new Map())
+
+  const transitions = useTransition(toasts, (toast) => toast.toast, {
+    enter: (item) => async (next) => {
+      await next({
+        opacity: 1,
+        height: refMap.get(item.toast).clientHeight + 16,
+      })
+    },
+    leave: (item) => async (next) => {
+      refMap.delete(item.toast)
+      await next({ opacity: 0, height: 0 })
+    },
+    from: { opacity: 0, height: 0 },
+  })
 
   return (
-    <ul
+    <div
       css={css`
         position: fixed;
         bottom: 16px;
         right: 16px;
-
-        li + li {
-          margin-top: 16px;
-        }
       `}
     >
-      {toasts.map((t) => (
-        <li>
-          <Toast key={t.toast} {...t} />
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {transitions.map(({ item, props, key }) => (
+          <animated.li key={key} style={props}>
+            <div ref={(ref) => ref && refMap.set(item.toast, ref)}>
+              <Toast key={item.toast} {...item} />
+            </div>
+          </animated.li>
+        ))}
+      </ul>
+    </div>
   )
 }
 

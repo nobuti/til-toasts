a11f4d12a82884c7338b28980463f7dc07a2fa1d
nobuti <buti@nobuti.com>
2020-04-11 08:39:40 +0200

    Fix setimeout test based on fake timers

--- src/components/__tests__/Toast.test.js
+++ src/components/__tests__/Toast.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import { advanceBy, advanceTo, clear } from 'jest-date-mock'
 
 import Toast from '../Toast'
 import * as ToastContext from '../toastContext'
+
+jest.useFakeTimers()
 
 describe('Toast', () => {
   const removeToast = jest.fn()
@@ -13,11 +14,9 @@ describe('Toast', () => {
 
   const toast = { color: '#2548f5', title: 'Wadus', message: 'wadus wadus!' }
 
-  beforeEach(() => {
-    advanceTo(new Date(2020, 3, 5, 0, 0, 0)) // reset to date time.
+  afterEach(() => {
+    removeToast.mockClear()
   })
-
-  afterEach(clear)
 
   it('should render properly', () => {
     const { container } = render(<Toast {...toast} />)
@@ -38,7 +37,7 @@ describe('Toast', () => {
   it('should hide after 5 seconds', async () => {
     render(<Toast {...toast} />)
 
-    advanceBy(6000) // advance time 6 seconds
+    jest.advanceTimersByTime(6000) // advance time 6 seconds
 
     expect(removeToast).toHaveBeenCalled()
   })
